24c38110f9d13f632b878a54f479af287f5e02c0
unknown <alan.waret24@gmail.com>
2022-11-09 11:44:26 +0100

    Steg audio (Byte seeing)

--- Fonctions/steg_spectre.py
+++ Fonctions/steg_spectre.py
@@ -1,2 +1,8 @@
-def steg_spectre(file, message):
-    pass
+import wave
+
+def wav_byte(file):
+    t = wave.open(file,"r")
+    gm_bytes = wave.Wave_read.readframes(t, 5)
+    return(gm_bytes)
+
+print(wav_byte(r"C:\Users\alanw\saphie-project\Fonctions\test_audio.wav"))
