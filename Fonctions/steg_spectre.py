e36e93262e2a894fb7703cc6a15951793a4c3055
unknown <alan.waret24@gmail.com>
2022-11-09 17:07:43 +0100

    Audio + Organisation

--- Fonctions/steg_spectre.py
+++ Fonctions/steg_spectre.py
@@ -1,8 +1,20 @@
+
 import wave
 
-def wav_byte(file):
-    t = wave.open(file,"r")
-    gm_bytes = wave.Wave_read.readframes(t, 5)
-    return(gm_bytes)
+def audio_message(file, message):
+    song = wave.open(file, mode='rb')
+    frame_bytes = bytearray(list(song.readframes(song.getnframes())))
+    string = message
+    string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
+    bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in string])))
 
-print(wav_byte(r"C:\Users\alanw\saphie-project\Fonctions\test_audio.wav"))
+    for i, bit in enumerate(bits):
+        frame_bytes[i] = (frame_bytes[i] & 254) | bit
+    frame_modified = bytes(frame_bytes)
+
+    with wave.open('Nouvo son.wav', 'wb') as fd:
+        fd.setparams(song.getparams())
+        fd.writeframes(frame_modified)
+    song.close()
+
+print(audio_message(r'C:\Users\alanw\saphie-project\Fonctions\Outro INSTRU.wav'), "OUAIIS")
