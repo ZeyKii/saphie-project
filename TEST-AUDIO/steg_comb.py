170d26fb81e96463af24d8f537171038500134ce
unknown <alan.waret24@gmail.com>
2023-01-04 12:16:04 +0100

    Rangement du repo

--- TEST-AUDIO/steg_comb.py
+++
@@ -1,8 +0,0 @@
-from pydub import AudioSegment
-
-sound1 = AudioSegment.from_file(r"C:\Users\maxfe\saphie-project\TEST-AUDIO\electro_jazz.wav")
-sound2 = AudioSegment.from_file(r"C:\Users\maxfe\saphie-project\TEST-AUDIO\Outro INSTRU.wav")
-
-combined = sound1.overlay(sound2)
-
-combined.export(r"C:\Users\maxfe\saphie-project\TEST-AUDIO\combined.wav", format='wav')
