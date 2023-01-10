e2ef40c07cb895ad3e1cef17585b8856f4f384e5
unknown <alan.waret24@gmail.com>
2023-01-11 00:21:06 +0100

    Ajout Fonctionnalité

--- Fonctions/steg_image.py
+++ Fonctions/steg_image.py
@@ -1,58 +1,68 @@
 from PIL import Image
 
-def stegano(name_img , msg):
-    im = Image.open(name_img)
-    # on récupère les dimensions de l'image
-    w , h = im.size
-    # on sépare l'image en trois : rouge, vert et bleu
-    r , g , b = im.split()
-    # on transforme la partie rouge en liste
-    r = list( r.getdata() )
-    # on calcule la longueur de la chaîne et on la transforme en binaire
-    u = len(msg)
-    v = bin( len(msg) )[2:].rjust(8,"0")
-    # on transforme la chaîne en une liste de 0 et de 1 
-    ascii = [ bin(ord(x))[2:].rjust(8,"0") for x in msg ]
-    # transformation de la liste en chaîne
-    a = ''.join(ascii)
-    # on code la longueur de la liste dans les 8 premiers pixels rouges
-    for j in range(8):
-        r[j] = 2 * int( r[j] // 2 ) + int( v[j] )
-    # on code la chaîne dans les pixels suivants
-    for i in range(8*u):
-        r[i+8] = 2 * int( r[i+8] // 2 ) + int( a[i] )
-        
-    # on recrée l'image rouge 
-    nr = Image.new("L",(16*w,16*h))
-    nr = Image.new("L",(w,h))
-    nr.putdata(r)
-    # fusion des trois nouvelles images
-    imgnew = Image.merge('RGB',(nr,g,b))
-    new_name_img = "couv_" + name_img
-    imgnew.save(new_name_img)
+def encode(image, message):
+  # Convertir l'image en objet pixel map
+  pixel_map = image.load()
 
-print(stegano("nightcity.jpeg" , "un monde futuriste"))
+  # Récupérer la taille de l'image
+  (width, height) = image.size
 
-def get_msg(name_couv):
-    im = Image.open(name_couv)
-    r , g , b = im.split()
-    r = list( r.getdata() )
-    
-    # lecture de la longueur de la chaine
-    p = [ str(x%2) for x in r[0:8] ]
-    q = "".join(p)
-    q = int(q,2)
-    
-    # lecture du message
-    n = [ str(x%2) for x in r[8:8*(q+1)] ]
+  # Convertir le message en binaire
+  binary_message = bin(int.from_bytes(message.encode(), 'big'))[2:]
 
-    b = "".join(n)
-    message = ""
+  # Ajouter des "0" jusqu'à ce que la longueur du message binaire soit un multiple de 3
+  while len(binary_message) % 3 != 0:
+    binary_message = "0" + binary_message
 
-    for k in range(0,q):
-        l = b[8*k:8*k+8]
-        message += chr(int(l,2))
-        
-    return message
-print(get_msg("couv_nightcity.jpeg"))
+  # Cacher le message binaire dans l'image
+  for i in range(0, len(binary_message), 3):
+    # Récupérer les 3 bits du message
+    bits = binary_message[i:i+3]
 
+    # Récupérer l'index du pixel à modifier
+    x = i // 3 % width
+    y = i // 3 // width
+
+    # Récupérer la valeur du pixel
+    (r, g, b) = pixel_map[x, y]
+
+    # Modifier les 3 bits de poids faible des composantes RGB
+    pixel_map[x, y] = (r // 2 * 2 + int(bits[0]), g // 2 * 2 + int(bits[1]), b // 2 * 2 + int(bits[2]))
+
+def decode(image):
+  # Convertir l'image en objet pixel map
+  pixel_map = image.load()
+
+  # Récupérer la taille de l'image
+  (width, height) = image.size
+
+  # Récupérer le message binaire caché dans l'image
+  binary_message = ""
+  for y in range(height):
+    for x in range(width):
+      # Récupérer la valeur du pixel
+      (r, g, b) = pixel_map[x, y]
+
+      # Récupérer les 3 bits de poids faible des composantes RGB
+      binary_message += str(r % 2)
+      binary_message += str(g % 2)
+      binary_message += str(b % 2)
+
+  # Supprimer les "0" ajoutés lors de l'encodage
+  binary_message = binary_message.rstrip("0")
+
+  # Convertir le message binaire en chaîne de caractères
+  message = int(binary_message, 2).to_bytes((len(binary_message) + 7) // 8, 'big').decode()
+
+  return message
+
+def main():
+  # Ouvrir l'image
+  image = Image.open(r"C:\Users\alanw\saphie-project\Fonctions\Image\TEST.png")
+
+  # Encoder un message dans l'image
+  encode(image, "Ceci est un message secret !")
+
+  # Sauvegarder l'image modifiée
+  image.save("image_modified.png")
+main()
