340fd9d7156f8ac8b57f1c116bc3cce0f07a7094
Kzenoo <guilhem.raffanel@ynov.com>
2023-03-08 11:32:50 +0100

    creation of web page

--- main/main.go
+++ main/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	saphie "saphie/views"
+	"text/template"
 )
 
 func main() {
+	http.HandleFunc("/", Index)
 	fmt.Println("Server web lauch on http://localhost:8080/")
 	saphie.StartServer()
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./static/index.html"))
+	tmpl.Execute(w, nil)
 }
