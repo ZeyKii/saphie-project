745e1e59a40a677c116cb92a766b4441af71ed3e
Alan Bourdoncle <alan.waret24@gmail.com>
2023-03-08 09:51:58 +0100

    Arborescence

--- views/WebManager.go
+++ views/WebManager.go
@@ -7,20 +7,5 @@ func StartServer() {
 	http.Handle("/dist/", http.StripPrefix("/dist/", fs))
 	img := http.FileServer(http.Dir("../assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", img))
-
-	http.HandleFunc("/redirect", gotoAuth)
-	http.HandleFunc("/authentification", homeHandler)
-	http.HandleFunc("/register", Register)
-	http.HandleFunc("/login", Login)
-
-	http.HandleFunc("/", Forum)
-
-	http.HandleFunc("/topic", Topic)
-	http.HandleFunc("/posting", doPost)
-	http.HandleFunc("/post", TmpPost)
-	http.HandleFunc("/comment_add", doComment)
-
-	http.HandleFunc("/profile", Profile)
-	http.HandleFunc("/upload", UploadFile)
 	http.ListenAndServe(":8080", nil)
 }
