ed0aef29bce3685670568f8d4c0614c25f4ec950
Alan Bourdoncle <alan.waret24@gmail.com>
2023-03-08 10:28:38 +0100

    Server

--- views/WebManager.go
+++ views/WebManager.go
@@ -1,11 +1,19 @@
 package saphie
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func StartServer() {
-	fs := http.FileServer(http.Dir("../static/dist"))
-	http.Handle("/dist/", http.StripPrefix("/dist/", fs))
-	img := http.FileServer(http.Dir("../assets"))
-	http.Handle("/assets/", http.StripPrefix("/assets/", img))
-	http.ListenAndServe(":8080", nil)
+	fs := http.FileServer(http.Dir("./static/dist/"))
+	http.Handle("/static/dist/", http.StripPrefix("/static/dist/", fs))
+	http.ListenAndServe(":80", nil)
+
+	http.HandleFunc("/", Main_Page)
+}
+
+func Main_Page(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseFiles("/static/dist/index.html"))
+	tpl.Execute(w, nil)
 }
