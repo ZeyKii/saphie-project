9cbc0396165771c92ee31e2893c30fa117abd0f8
Maxance Ferran <maxance.ferran@ynov.com>
2023-04-05 16:05:47 +0200

    fix

--- views/WebManager.go
+++ views/WebManager.go
@@ -16,10 +16,6 @@ func StartServer() {
 }
 
 func Main_Page(w http.ResponseWriter, r *http.Request) {
-<<<<<<< HEAD
 	tpl := template.Must(template.ParseFiles("/static/dist/steg-image.html"))
-=======
-	tpl := template.Must(template.ParseFiles("/static/index.html"))
->>>>>>> 177c9304e7de0bdfc7e1e351ef69e8ac956a1c82
 	tpl.Execute(w, nil)
 }
